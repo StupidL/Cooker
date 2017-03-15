a643af4fdd27ce555d555865289a307745fd232b
StupidL <562117676@qq.com>
2017-03-15 15:34:29 +0800

    update AboutActivity and replace logo

--- app/src/main/java/me/stupidme/cooker/view/AboutActivity.java
+++ app/src/main/java/me/stupidme/cooker/view/AboutActivity.java
@@ -1,16 +1,16 @@
 package me.stupidme.cooker.view;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import me.stupidme.cooker.R;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Dialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +23,20 @@ public class AboutActivity extends AppCompatActivity {
             toolbar.setNavigationOnClickListener(v -> finish());
         }
 
+        mDialog = new Dialog(this);
+        mDialog.setContentView(R.layout.dialog_about);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(
-                view -> Snackbar.make(view, "This project is opened source on Github", Snackbar.LENGTH_LONG)
-                        .setAction("Github", v -> {
-                            Intent intent = new Intent();
-                            intent.setAction(Intent.ACTION_VIEW);
-                            Uri content_url = Uri.parse("https://github.com/StupidL/Cooker");
-                            intent.setData(content_url);
-                            startActivity(Intent.createChooser(intent, "Please choose a browser"));
-                        }).show());
+//        fab.setOnClickListener(
+//                view -> Snackbar.make(view, "This project is opened source on Github", Snackbar.LENGTH_LONG)
+//                        .setAction("Github", v -> {
+//                            Intent intent = new Intent();
+//                            intent.setAction(Intent.ACTION_VIEW);
+//                            Uri content_url = Uri.parse("https://github.com/StupidL/Cooker");
+//                            intent.setData(content_url);
+//                            startActivity(Intent.createChooser(intent, "Please choose a browser"));
+//                        }).show());
+
+        fab.setOnClickListener(v -> mDialog.show());
     }
 }
