36e14a2eab9c9b31221ead4da3c6d4944b912da5
StupidL <562117676@qq.com>
2017-03-19 13:34:41 +0800

    add annotations, which may be removed later

--- app/src/main/java/me/stupidme/cooker/annotation/Column.java
+++ app/src/main/java/me/stupidme/cooker/annotation/Column.java
@@ -1,8 +1,26 @@
 package me.stupidme.cooker.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by StupidL on 2017/3/18.
  */
 
-public interface Column {
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+    int columnSize() default 0;
+
+    ColumnType columnType() default ColumnType.STRING;
+
+    Constraints constraints() default @Constraints;
+
+    enum ColumnType {
+        STRING,
+        INTEGER,
+        FLOAT
+    }
 }
--- app/src/main/java/me/stupidme/cooker/annotation/Table.java
+++ app/src/main/java/me/stupidme/cooker/annotation/Table.java
@@ -1,8 +1,16 @@
 package me.stupidme.cooker.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by StupidL on 2017/3/18.
  */
 
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Table {
+    String name() default "";
 }
