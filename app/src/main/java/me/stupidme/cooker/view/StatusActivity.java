5037c74d9bc758da5d35a39351b67b11e49cb5f5
StupidL <562117676@qq.com>
2017-03-26 22:43:45 +0800

    create StatusActivity

---
+++ app/src/main/java/me/stupidme/cooker/view/StatusActivity.java
@@ -0,0 +1,15 @@
+package me.stupidme.cooker.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.stupidme.cooker.R;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
