0c97b974c29c93c3055956b11f8949852f2bdcc2
StupidL <562117676@qq.com>
2017-04-02 19:10:46 +0800

    create base activity and fragment

--- app/src/main/java/me/stupidme/cooker/view/base/BaseFragment.java
+++ app/src/main/java/me/stupidme/cooker/view/base/BaseFragment.java
@@ -1,10 +1,30 @@
 package me.stupidme.cooker.view.base;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by StupidL on 2017/4/2.
  */
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initView(View view, Bundle savedInstanceState);
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), parent, false);
+        initView(view, savedInstanceState);
+        return view;
+    }
+
+    protected void showToast(CharSequence message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
 }
