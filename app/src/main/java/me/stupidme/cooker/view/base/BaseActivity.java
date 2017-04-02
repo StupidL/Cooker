b939d1a1196687a2d6ec2d2000f0b7f728103beb
StupidL <562117676@qq.com>
2017-04-02 19:10:16 +0800

    create base activity and fragment

---
+++ app/src/main/java/me/stupidme/cooker/view/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package me.stupidme.cooker.view.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by StupidL on 2017/4/2.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/me/stupidme/cooker/view/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package me.stupidme.cooker.view.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by StupidL on 2017/4/2.
+ */
+
+public class BaseFragment extends Fragment {
+}
