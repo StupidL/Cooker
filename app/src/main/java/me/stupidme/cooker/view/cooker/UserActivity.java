8eb125a93cb874852fc47bd84e4f97a1a2b16ba6
StupidL <562117676@qq.com>
2017-04-08 14:54:56 +0800

    create UserActivity and update ChoosePicDialog

--- app/src/main/java/me/stupidme/cooker/view/cooker/UserActivity.java
+++ app/src/main/java/me/stupidme/cooker/view/cooker/UserActivity.java
@@ -1,15 +1,65 @@
 package me.stupidme.cooker.view.cooker;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import me.stupidme.cooker.R;
+import me.stupidme.cooker.view.base.BaseActivity;
+import me.stupidme.cooker.view.custom.ChoosePicDialog;
 
-public class UserActivity extends AppCompatActivity {
+/**
+ * Created by StupidL 2017/4/8.
+ */
+
+public class UserActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+    protected int getContentViewId() {
+        return R.layout.activity_user;
+    }
+
+    @Override
+    protected void handleIntent() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(v -> {
+                setResult(RESULT_OK);
+                finish();
+            });
+        }
+
+        CircleImageView mCircleImage = (CircleImageView) findViewById(R.id.user_head_big);
+        TextView mTextView = (TextView) findViewById(R.id.user_name);
+        mTextView.setClickable(false);
+        mTextView.setFocusable(false);
+
+        ChoosePicDialog.ChoosePicListener mListener = new ChoosePicDialog.ChoosePicListener() {
+            @Override
+            public void chooseFromAlbum() {
+
+            }
+
+            @Override
+            public void takeFromCamera() {
+
+            }
+        };
+        mCircleImage.setOnClickListener(v ->
+                new ChoosePicDialog(this, mListener).show());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 }
--- app/src/main/java/me/stupidme/cooker/view/custom/ChoosePicDialog.java
+++ app/src/main/java/me/stupidme/cooker/view/custom/ChoosePicDialog.java
@@ -3,6 +3,7 @@ package me.stupidme.cooker.view.custom;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.Window;
 import android.widget.TextView;
 
 import me.stupidme.cooker.R;
@@ -18,14 +19,23 @@ public class ChoosePicDialog extends Dialog {
     public ChoosePicDialog(@NonNull Context context, ChoosePicListener listener) {
         super(context);
 
+        if (getWindow() != null)
+            getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.dialog_change_user_head);
         TextView mAlbumTextView = (TextView) findViewById(R.id.choose_from_album);
         TextView mCameraTextView = (TextView) findViewById(R.id.take_from_camera);
         mListener = listener;
 
-        mAlbumTextView.setOnClickListener(v -> mListener.chooseFromAlbum());
+        mAlbumTextView.setOnClickListener(v -> {
+            mListener.chooseFromAlbum();
+            dismiss();
+        });
 
-        mCameraTextView.setOnClickListener(v -> mListener.takeFromCamera());
+        mCameraTextView.setOnClickListener(v -> {
+            mListener.takeFromCamera();
+            dismiss();
+        });
     }
 
     public interface ChoosePicListener {
